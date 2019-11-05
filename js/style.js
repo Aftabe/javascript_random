01dd05b99199c71dbf8d49ea3ef84225e3ad2b76
Ali Khan <shamsasghari77@gmail.com>
2019-11-05 21:48:39 +1100

    background added

--- js/style.js
+++ js/style.js
@@ -2,7 +2,7 @@
 // document.getElementById('test').innerHTML = Math.PI;
 
 // document.getElementById('test').innerHTML = Math.round(23.5);
-
+document.body.style.backgroundColor = "grey";
 
 function myFunction(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
