2df93bba7366dc125f81c3a160ff8d0aeadd7eca
Ali Khan <shamsasghari77@gmail.com>
2019-11-05 21:47:09 +1100

    files added

---
+++ js/style.js
@@ -0,0 +1,9 @@
+// This lesson is all about javaScript Math
+// document.getElementById('test').innerHTML = Math.PI;
+
+// document.getElementById('test').innerHTML = Math.round(23.5);
+
+
+function myFunction(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
